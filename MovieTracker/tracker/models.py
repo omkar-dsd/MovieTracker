a7010b8beeb311146b3520ba333b486a9cbfb52f
Omkar DEshpande <omkar.dsd@gmail.com>
2017-09-23 18:07:47 +0530

    User Watched Movies Form generated

--- MovieTracker/tracker/models.py
+++ MovieTracker/tracker/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class UserWatched(models.Model):
+    username = models.CharField(max_length=100)
+    movie_id = models.IntegerField()
+    movie_rating = models.IntegerField()
--- MovieTracker/tracker/views.py
+++ MovieTracker/tracker/views.py
@@ -4,6 +4,27 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.forms import UserCreationForm
 from MovieTracker import settings
 from django.contrib.auth.decorators import login_required
+from django import forms
+
+
+
+# Keys:
+#
+# API Key (v3 auth)
+# ca59847d10d1b5321571a0a279c95e61
+# API Read Access Token (v4 auth)
+# eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYTU5ODQ3ZDEwZDFiNTMyMTU3MWEwYTI3OWM5NWU2MSIsInN1YiI6IjU5YzYzN2E2OTI1MTQxNWI2ZTAzZDc4NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5pcWArPylqn1BxS5Lgsdn90li5_YfXD34OPhYG76-uY
+# example request
+# https://api.themoviedb.org/3/movie/550?api_key=ca59847d10d1b5321571a0a279c95e61
+
+# Example Image url
+# https://image.tmdb.org/t/p/w150/nl79FQ8xWZkhL3rDr1v2RFFR6J0.jpg
+
+#Get movie detail
+# https://api.themoviedb.org/3/movie/278?api_key=ca59847d10d1b5321571a0a279c95e61&language=en-US
+class ColorForm(forms.Form):
+    fav_color = forms.CharField(label='Fav Color', max_length=100)
+
 
 def Login(request):
     next = request.GET.get('next', '/home/')
@@ -31,7 +52,26 @@ def Logout(request):
 # login is required for the following function Home
 @login_required
 def Home(request):
-    return render(request, "tracker/home.html", {})
+    if request.method == 'POST':
+
+        form = ColorForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            request.session["fav_color"] = request.POST['fav_color']
+
+            # return HttpResponseRedirect('/home/')
+    else:
+        form = ColorForm()
+
+    if(not "fav_color" in request.session.keys()):
+        # print "relksal;dfkm"
+        request.session["fav_color"] = "Empty"
+
+    return render(request, "tracker/home.html", {'fav_color':request.user,
+                                                 'form':form})
 
 def Blog(request):
     return render(request, "tracker/blog.html", {})
