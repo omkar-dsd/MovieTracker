48ed50155a127f8fb1f5b57cda9b5e7e8b9a1aa5
omkar-dsd <omkar-dsd@gitlab.com>
2017-09-25 03:08:16 +0530

    Watched Section Update

--- MovieTracker/tracker/urls.py
+++ MovieTracker/tracker/urls.py
@@ -3,9 +3,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^login/$', views.Login),
-    url(r'^logout/$', views.Logout),
-    url(r'^watched/$', views.Watched),
-    url(r'^signup/$', views.Signup),
-    url(r'^unwatched/$', views.Unwatched),
+    url(r'^login/$', views.Login, name='login'),
+    url(r'^logout/$', views.Logout, name='logout'),
+    url(r'^watched/$', views.Watched, name='watched'),
+    url(r'^signup/$', views.Signup, name='signup'),
+    url(r'^unwatched/$', views.Unwatched, name='unwatched'),
+    url(r'^watched/(?P<movieid>\d+)/$', views.AddToWatched, name='watchedMovie'),
 ]
--- MovieTracker/tracker/views.py
+++ MovieTracker/tracker/views.py
@@ -5,8 +5,11 @@ from django.contrib.auth.forms import UserCreationForm
 from MovieTracker import settings
 from django.contrib.auth.decorators import login_required
 from django import forms
+from tracker.models import UserWatched
 import requests
 import json
+from django.core.urlresolvers import reverse
+
 
 
 
@@ -29,7 +32,7 @@ class ColorForm(forms.Form):
 
 
 def Login(request):
-    next = request.GET.get('next', '/watched/')
+    next = request.GET.get('next', '/unwatched/')
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
@@ -54,27 +57,41 @@ def Logout(request):
 # login is required for the following function Watched
 @login_required
 def Watched(request):
-    if request.method == 'POST':
+    # if request.method == 'POST':
+    #
+    #     form = ColorForm(request.POST)
+    #     # check whether it's valid:
+    #     if form.is_valid():
+    #         # process the data in form.cleaned_data as required
+    #         # ...
+    #         # redirect to a new URL:
+    #         request.session["fav_color"] = request.POST['fav_color']
+    #
+    #         # return HttpResponseRedirect('/watched/')
+    # else:
+    #     form = ColorForm()
+    #
+    # if(not "fav_color" in request.session.keys()):
+    #     # print "relksal;dfkm"
+    #     request.session["fav_color"] = "Empty"
 
-        form = ColorForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            request.session["fav_color"] = request.POST['fav_color']
-
-            # return HttpResponseRedirect('/watched/')
+    watchedMovieIds = UserWatched.objects.values_list('movie_id', flat=True).filter(username=request.user)
+    if(not watchedMovieIds):
+        movies_list = None
     else:
-        form = ColorForm()
-
-    if(not "fav_color" in request.session.keys()):
-        # print "relksal;dfkm"
-        request.session["fav_color"] = "Empty"
+        movies_list = getWatchedMovies(set(watchedMovieIds))
 
     return render(request, "tracker/watched.html", {'current_user':request.user,
-                                                 'form':form})
+                                                    'movies_list' : movies_list})
 
+@login_required
+def AddToWatched(request, movieid):
+
+
+    b = UserWatched(username=request.user, movie_id=movieid, movie_rating=10)
+    b.save()
+
+    return HttpResponseRedirect(reverse('unwatched'))
 
 @login_required
 def Unwatched(request,pageNumber=1):
@@ -96,7 +113,7 @@ def Unwatched(request,pageNumber=1):
         # print "relksal;dfkm"
         request.session["fav_color"] = "Empty"
 
-    movies_list = getTopRated(pageNumber)
+    movies_list = getTopRated(request,pageNumber)
     return render(request, "tracker/unwatched.html", {'current_user':request.user,
                                                       'movies_list' : movies_list})
 
@@ -123,10 +140,6 @@ def Movie(request):
     return render(request, "tracker/unwatched.html", {'current_user':request.user})
 
 
-def Blog(request):
-    return render(request, "tracker/blog.html", {})
-
-
 def Signup(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -144,14 +157,39 @@ def Signup(request):
     return render(request, 'tracker/signup.html', {'form': form})
 
 
-def getTopRated(pageNumber):
-    r = requests.get("https://api.themoviedb.org/3/movie/top_rated?api_key=ca59847d10d1b5321571a0a279c95e61&language=en-US&page="+str(pageNumber))
-    movies = json.loads(r.text)
+def getTopRated(request,pageNumber):
 
-    if pageNumber>movies['total_pages']:
-        return None
-    else:
-        for i in range(len(movies['results'])):
-            movies['results'][i]['poster_path'] = "https://image.tmdb.org/t/p/w150" + movies['results'][i]['poster_path']
+    watchedMovieIds = UserWatched.objects.values_list('movie_id', flat=True).filter(username=request.user)
 
-        return movies['results']
+    god = []
+
+    while(len(god)<100):
+        r = requests.get("https://api.themoviedb.org/3/movie/top_rated?api_key=ca59847d10d1b5321571a0a279c95e61&language=en-US&page="+str(pageNumber))
+        movies = json.loads(r.text)
+        if pageNumber>movies['total_pages']:
+            return god
+        else:
+            for i in range(len(movies['results'])):
+                if(movies['results'][i]['id'] in watchedMovieIds):
+                    continue
+                else:
+                    movies['results'][i]['poster_path'] = "https://image.tmdb.org/t/p/w150" + movies['results'][i]['poster_path']
+                    god.append(movies['results'][i])
+
+        pageNumber+=1
+
+    return god
+
+def getWatchedMovies(idList):
+
+    results = []
+
+    for id in idList:
+        url = "https://api.themoviedb.org/3/movie/" + str(id) + "?api_key=ca59847d10d1b5321571a0a279c95e61&language=en-US"
+        r = requests.get(url)
+        movie = json.loads(r.text)
+        movie['poster_path'] = "https://image.tmdb.org/t/p/w150" + movie['poster_path']
+        results.append(movie)
+
+
+    return results
