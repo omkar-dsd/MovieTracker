c664574434dc6a1fe277ee7154b3a4a035f10969
omkar-dsd <omkar-dsd@gitlab.com>
2017-09-27 15:33:43 +0530

    whitenoise error

--- MovieTracker/MovieTracker/settings.py
+++ MovieTracker/MovieTracker/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
 )
 
 ROOT_URLCONF = 'MovieTracker.urls'
