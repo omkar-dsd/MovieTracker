57679af8932505c260756ac3340075b9b21ae47f
omkar-dsd <omkar-dsd@gitlab.com>
2017-09-27 19:18:15 +0530

    Deployed on heroku'

--- MovieTracker/MovieTracker/settings.py
+++ MovieTracker/MovieTracker/settings.py
@@ -102,9 +102,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT = os.path.join(BASE_DIR, "/tracker/static")
+STATIC_ROOT = os.path.join(BASE_DIR, "tracker/static")
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 
